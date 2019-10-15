2a8980b79f975cde3fd7c58a15523f6863ea394d
eric-couch <James.Couch@edgetechacademy.edu>
2019-10-14 21:29:21 -0500

    Final Exam Lab Initial Code

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// Do all of these items listed when the user click the submit button
+
+// Validate Name to make sure it isn't empty.  Issue a bold and red statement in NameMessage span if it is.
+
+// Place message in GenderMessage span of radio button indicating required field if left blank.
+
+// Validate citizenship status.  Message in the CitizenMessage span if not a citizen and exit function
+
+// check the user's age.  Issue an alert if they are under 18 and not eligible to vote
+
+// Place a message in the span of the select form if no county is selected
+
+// make sure email is not left blank and provide message if it is
